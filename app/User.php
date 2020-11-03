52fdc2e9693ec15b8a2800a19da23295b13e5b91
Manuel Henriquez <manuelhm1993@gmail.com>
2020-11-03 19:05:27 -0400

    Iniciando nuevamente
    Se reinició el proyecto

--- app/User.php
+++ app/User.php
@@ -2,16 +2,13 @@
 
 namespace App;
 
-//Illuminate\Contracts\Auth\Access\Authorizable; //Se debe implementar esta interfaz
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-//use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     use Notifiable;
-    //use HasRoles;
 
     /**
      * The attributes that are mass assignable.
