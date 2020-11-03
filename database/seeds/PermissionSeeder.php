c20d50ffb93c4ac9f38b6d6023f873a9d96e1cb8
Manuel Henriquez <manuelhm1993@gmail.com>
2020-11-03 02:19:38 -0400

    Se crearon los permisos de usuario

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use Caffeinated\Shinobi\Models\Permission;
+
 class PermissionSeeder extends Seeder
 {
     /**
@@ -11,6 +13,35 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Permission::create([
+            [
+                'name' => 'Listar usuarios',
+                'slug' => 'users.index',//En el slug se asigna el permiso sobre la ruta
+                'description' => 'Lista y navega todos los usuarios del sistema',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Ver usuario',
+                'slug' => 'users.show',
+                'description' => 'Ver el resumen detallado de cada usuario del sistema',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Editar usuario',
+                'slug' => 'users.edit',
+                'description' => 'Editar cualquier dato de un usuario del sistema',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Eliminar usuario',
+                'slug' => 'users.destroy',
+                'description' => 'Eliminar cualquier usuario del sistema',
+                'created_at' => now(),
+                'updated_at' => now()
+            ]
+        ]);
     }
 }
