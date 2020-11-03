f0bd70c8deadb81ea1788919762d7dab02c92b62
Manuel Henriquez <manuelhm1993@gmail.com>
2020-11-03 02:22:23 -0400

    Se creó un Seeder para llenar la tabla de roles

---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Caffeinated\Shinobi\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            [
+                'name' => 'Listar usuarios',
+                'slug' => 'users.index',//En el slug se asigna el permiso sobre la ruta
+                'description' => 'Lista y navega todos los usuarios del sistema',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Ver usuario',
+                'slug' => 'users.show',
+                'description' => 'Ver el resumen detallado de cada usuario del sistema',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Editar usuario',
+                'slug' => 'users.edit',
+                'description' => 'Editar cualquier dato de un usuario del sistema',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Eliminar usuario',
+                'slug' => 'users.destroy',
+                'description' => 'Eliminar cualquier usuario del sistema',
+                'created_at' => now(),
+                'updated_at' => now()
+            ]
+        ]);
+    }
+}
