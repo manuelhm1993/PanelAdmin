69170cee2d13f42a296a6e5b60f55a4ffd72c367
Manuel Henriquez <manuelhm1993@gmail.com>
2020-11-05 13:37:08 -0400

    Se agregó el mensaje de feedback

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,6 +89,22 @@
             </div>
         </nav>
 
+        @if (session('info'))
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 offset-md-2">
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <strong>¡Mensaje!</strong> {{ session('info') }}
+            
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+
         <main class="py-4">
             @yield('content')
         </main>
