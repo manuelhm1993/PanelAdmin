a32841441559b44f7386e42ca08adb446de3102b
Manuel Henriquez <manuelhm1993@gmail.com>
2020-11-04 20:33:37 -0400

    - Se configuró el modelo User para el uso de roles y permisos - Se creó el seeder para sembrar permisos - Se ejecutaron las migraciones

---
+++ database/seeds/PermissionsSeeder.php
@@ -0,0 +1,109 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\User;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class PermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        //---------- Permisos para manejar usuario ----------//
+        Permission::create([
+            'name' => 'Listar usuarios',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Permission::create([
+            'name' => 'Ver usuario',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Permission::create([
+            'name' => 'Editar usuario',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Permission::create([
+            'name' => 'Eliminar usuario',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        //---------- Permisos para manejar roles ----------//
+        Permission::create([
+            'name' => 'Crear perfil',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Permission::create([
+            'name' => 'Listar perfiles',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Permission::create([
+            'name' => 'Ver perfil',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Permission::create([
+            'name' => 'Editar perfil',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Permission::create([
+            'name' => 'Eliminar perfil',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        //---------- Permisos para manejar productos ----------//
+        Permission::create([
+            'name' => 'Crear producto',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Permission::create([
+            'name' => 'Listar productos',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Permission::create([
+            'name' => 'Ver producto',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Permission::create([
+            'name' => 'Editar producto',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Permission::create([
+            'name' => 'Eliminar producto',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
