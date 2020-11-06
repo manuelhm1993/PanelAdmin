493c90be06c57e5d39ed7747f2ed1dfcc736caa5
Manuel Henriquez <manuelhm1993@gmail.com>
2020-11-06 03:06:04 -0400

    Se creó la lógica de controlador de usuarios Los usuarios tendrán todos los permisos excepto crear, esto se debe a que los usuarios se registran solos y una vez ingresados en sistema se les asignan los distintos roles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,51 +14,57 @@ class UserController extends Controller
      */
     public function index()
     {
-        return 'Prueba de listado de usuarios';
+        $users = User::paginate();
+
+        return view('users.index', compact('users'));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        //
+        return view('users.show', compact('user'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(User $user)
     {
-        //
+        return view('users.edit', compact('user'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $user->update($request->all());
+
+        return redirect()->route('users.index')->with('info', 'Usuario actualizado exitosamente');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\User  $user
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return redirect()->route('users.index')->with('info', 'Usuario eliminado exitosamente');
     }
 }
